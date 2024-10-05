0e7c62a32b4f12ef395f9ad769b8462b247e9584
lilgou <panumas22305@gmail.com>
2024-10-05 12:01:28 +0700

    edit: app.py

--- database/app.py
+++ database/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from DB import get_db_connection
+from database.DB import get_db_connection
 import os
 connection = get_db_connection()
 
