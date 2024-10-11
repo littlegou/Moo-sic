5058ca6515306c8d6bc4de7d0fd377469e9bb123
lilgou <panumas22305@gmail.com>
2024-10-11 11:55:47 +0700

    find embed link that bug again

--- database/app.py
+++ database/app.py
@@ -9,9 +9,9 @@ static_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static')
 app = Flask(__name__, template_folder=template_dir,static_folder=static_dir)
 app.secret_key = 'your_secret_key'
 
-@app.route('/')
-def main():
-    return render_template('index.html')
+# @app.route('/')
+# def main():
+#     return render_template('index.html')
 
 @app.route('/login', methods =['GET', 'POST'])
 def login():
@@ -112,12 +112,12 @@ def randomsong():
     print(songs)
     return render_template('randomsong.html', songs=songs)
 
-# @app.route('/')
-# def findbug():
-#     with connection.cursor() as cursor:
-#         cursor.execute('SELECT Embed FROM song')
-#         songs = cursor.fetchall()
-#     return render_template('findbug.html', songs=songs)
+@app.route('/')
+def findbug():
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT Embed FROM song')
+        songs = cursor.fetchall()
+    return render_template('findbug.html', songs=songs)
 
 @app.route('/profile')
 def profile():
