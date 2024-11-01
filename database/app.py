ff058117ed59db567a18f98b6f0685d25763cf4f
lilgou <panumas22305@gmail.com>
2024-11-02 01:32:18 +0700

    Revert "edit randomsong and add main page"
    This reverts commit 15999f91c1100b3d95a493364095c0237cf5bd1b.

--- database/app.py
+++ database/app.py
@@ -15,26 +15,6 @@ app.secret_key = 'your_secret_key'
 def main():
     return render_template('index.html')
 
-@app.route('/m')
-def m():
-    return render_template('m.html')
-
-@app.route('/settings', methods =['GET', 'POST'])
-def settings():
-    return render_template('settings.html')
-
-@app.route('/calendar', methods =['GET', 'POST'])
-def calendar():
-    return render_template('calendar.html')
-
-@app.route('/recommend', methods =['GET', 'POST'])
-def recommend():
-    return render_template('recommend.html')
-
-@app.route('/karaoke', methods =['GET', 'POST'])
-def karaoke():
-    return render_template('karaoke.html')
-
 @app.route('/login', methods =['GET', 'POST'])
 def login():
     mesage = ''
@@ -49,7 +29,7 @@ def login():
             session['loggedin'] = True
             session['username'] = user[0]
             mesage = 'Logged in successfully !'
-            return redirect(url_for('m'))
+            return render_template('selectmood.html', mesage = mesage)
         else:
             mesage = 'Please enter correct username / password !'
     return render_template('login.html', mesage = mesage)
@@ -82,16 +62,18 @@ def sign_up():
             mesage = 'Please fill you password more than 8 letters !'
         else:
             hashed_password = generate_password_hash(password)
+            print(username,hashed_password,1)
             cursor = connection.cursor(buffered=True)
             cursor.execute('INSERT INTO users (username, email, password) VALUES (%s, %s, %s)', (username, email, hashed_password))
             connection.commit()
             mesage = 'You have successfully registered !'
+            print(username,hashed_password)
             return render_template('login.html')
     elif request.method == 'POST':
         mesage = 'Please fill out the form !'
     return render_template('signup.html', mesage = mesage)
 
-@app.route('/select_mood', methods=['GET', 'POST'])
+@app.route('/select_mood')
 def select_mood():
     return render_template('selectmood.html')
 
@@ -105,125 +87,41 @@ def happy():
 
 @app.route('/randomsong', methods=['GET', 'POST'])
 def randomsong():
-    username = session.get('username')
-    favorite_songs = []
-    mood = request.args.get('mood')
-    reason = request.args.get('reason')
-    if (mood != session.get('mood') and mood) or (reason != session.get('reason') and reason):
-        session['mood'] = mood
-        session['reason'] = reason
-        session['current_song'] = []
-        print(session)
-    if 'refresh' in request.args:
-        print(session)
-        query = "SELECT mood, why FROM song WHERE song_name = %s"
-        songname = session.get('current_song')[0][0]
-        with connection.cursor() as cursor:
-            cursor.execute(query, (songname,))
-            md = cursor.fetchone()
-        print(session)
-        mood = md[0]
-        reason = md[1]
-        print(mood,reason)
-        if mood and reason:
-            query = "SELECT song_name, artist, embed FROM song WHERE mood = %s AND why = %s ORDER BY RAND() LIMIT 1"
-            with connection.cursor() as cursor:
-                cursor.execute(query, (mood, reason))
-                songs = cursor.fetchall()
-                session['current_song'] = songs
-    elif session['current_song'] == []:
-        print("sur")
-        mood = request.args.get('mood')
-        reason = request.args.get('reason')
-        if mood and reason:
-            session['mood'] = mood
-            session['reason'] = reason
-            
-            query = "SELECT song_name, artist, embed FROM song WHERE mood = %s AND why = %s ORDER BY RAND() LIMIT 1"
-            with connection.cursor() as cursor:
-                cursor.execute(query, (mood, reason))
-                songs = cursor.fetchall()
-
-            if songs:
-                session['current_song'] = songs
-                print(songs)
-
-    if username:
-        with connection.cursor() as cursor:
-            check_fav_query = "SELECT Song, Artists, yt FROM favorites WHERE username = %s"
-            cursor.execute(check_fav_query, (username,))
-            favorite_songs = cursor.fetchall()
-
     if request.method == 'POST':
-        if 'add_fav' in request.form:
-            song_name = request.form.get('song_name')
-            artist = request.form.get('artist')
-            query = "SELECT yt FROM song WHERE song_name = %s"
-            with connection.cursor() as cursor:
-                cursor.execute(query, (song_name,))
-                result = cursor.fetchone()
+        song_name = request.form.get('song_name')
+        artist = request.form.get('artist')
+        username = session.get('username')
+        print(song_name,username,artist)
+        query = "SELECT yt FROM song WHERE song_name = %s"
+        with connection.cursor() as cursor:
+            cursor.execute(query, (song_name,))
+            result = cursor.fetchone()
 
-                if result:
-                    youtube_link = result[0]
+            if result:
+                youtube_link = result[0]
 
-                    if username and song_name and artist:
-                        check_fav_query = "SELECT * FROM favorites WHERE username = %s AND Song = %s"
-                        cursor.execute(check_fav_query, (username, song_name))
-                        existing_favorite = cursor.fetchone()
+                if username and song_name and artist:
+                    check_fav_query = "SELECT * FROM favorites WHERE username = %s AND Song = %s"
+                    cursor = connection.cursor(buffered=True)
+                    cursor.execute(check_fav_query, (username, song_name))
+                    existing_favorite = cursor.fetchone()
 
-                        if not existing_favorite:
-                            insert_query = "INSERT INTO favorites (id, username, Song, Artists, yt) VALUES (NULL, %s, %s, %s, %s)"
+                    if not existing_favorite:
+                        insert_query = "INSERT INTO favorites (id, username, Song, Artists, yt) VALUES (NULL, %s, %s, %s, %s)"
+                        with connection.cursor() as cursor:
                             cursor.execute(insert_query, (username, song_name, artist, youtube_link))
                             connection.commit()
-                            with connection.cursor() as cursor:
-                                check_fav_query = "SELECT Song, Artists, yt FROM favorites WHERE username = %s"
-                                cursor.execute(check_fav_query, (username,))
-                                favorite_songs = cursor.fetchall()
-                    return render_template('randomsong.html', songs=session['current_song'][0], favorite_songs=favorite_songs)
-        elif 'add_song' in request.form:
-            new_song_name = request.form.get('new_song_name')
-            new_artist = request.form.get('new_artist')
-            youtube_link = request.form.get('youtube_link')
-            new_mood = request.form.get('mood')
-            new_why = request.form.get('reason')
-            if "&list=" in youtube_link:
-                a = youtube_link[:youtube_link.find("&list=")]
-            else:
-                a = youtube_link
-            if "https://www.youtube.com/watch?v=" in a:
-                a = a.replace("https://www.youtube.com/watch?v=","")
-            elif "&list=" in a:
-                a = a[:a.find("&list=")]
-            elif "https://youtu.be/" in a:
-                a = a.replace("https://youtu.be/","")
-            else:
-                a = a.replace("https://www.youtube.com/","")
-            new_embed = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/"+ a + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
-            check_query = "SELECT * FROM song WHERE song_name = %s"
-            with connection.cursor() as cursor:
-                cursor.execute(check_query, (new_song_name,))
-                existing_song = cursor.fetchone()
-
-            if existing_song:
-                flash('เพลงนี้มีอยู่แล้ว!')
-            else:
-                if youtube_link:
-                    insert_query = "INSERT INTO song (song_id, song_name, Artist, Mood, Why, yt, Embed) VALUES (NULL, %s, %s, %s, %s, %s, %s)"
-                    with connection.cursor() as cursor:
-                        cursor.execute(insert_query, (new_song_name, new_artist, new_mood, new_why, youtube_link, new_embed))
-                        connection.commit()
-                flash('เพิ่มเพลงใหม่เรียบร้อย!')
-            return render_template('randomsong.html', songs=session['current_song'][0], favorite_songs=favorite_songs)
+        return redirect(url_for('profile'))
 
     mood = request.args.get('mood')
-    reason = request.args.get('reason')
-    session['mood'] = mood
-    session['reason'] = reason
-    print(session)
-    print(mood,reason)
-    current_song = session.get('current_song')
-    print(current_song)
-    return render_template('randomsong.html', songs=current_song[0], favorite_songs=favorite_songs)
+    why = request.args.get('why')
+
+    query = "SELECT song_name, artist, embed FROM song WHERE mood = %s AND why = %s ORDER BY RAND() LIMIT 1"
+    with connection.cursor() as cursor:
+        cursor.execute(query, (mood, why))
+        songs = cursor.fetchall()
+    print(songs)
+    return render_template('randomsong.html', songs=songs)
 
 @app.route('/profile')
 def profile():
